a74852538417b732fae7eb84d0286f6f8cb659a4
HuyHoang1605 <hoangnhse185092@fpt.edu.vn>
2025-06-03 19:32:38 +0700

    project init

---
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/SchoolMedicalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.be_source.School_Medical_Management_System_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMedicalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMedicalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ School-Medical-Management-System-/src/test/java/com/be_source/School_Medical_Management_System_/SchoolMedicalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be_source.School_Medical_Management_System_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMedicalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
