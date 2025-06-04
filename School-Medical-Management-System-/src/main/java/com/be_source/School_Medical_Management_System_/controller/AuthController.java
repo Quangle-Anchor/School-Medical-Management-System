55e5312b61b9022a38b19f4da834dc714b84ce7c
Khoa-213 <khoahass181074@fpt.edu.vn>
2025-06-04 15:42:03 +0700

    Login

---
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.be_source.School_Medical_Management_System_.controller;
+
+import com.be_source.School_Medical_Management_System_.model.User;
+import com.be_source.School_Medical_Management_System_.request.LoginRequest;
+import com.be_source.School_Medical_Management_System_.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        Optional<User> user = authService.login(request);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(401).body("Invalid email or password");
+        }
+    }
+}
