1be9124c819fe21680f7f3602f09a7ef3011032a
Khoa-213 <khoahass181074@fpt.edu.vn>
2025-06-04 08:48:09 +0700

    Repo

---
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/repository/PasswordRecoveryRepository.java
@@ -0,0 +1,8 @@
+package com.be_source.School_Medical_Management_System_.repository;
+
+import com.be_source.School_Medical_Management_System_.model.PasswordRecovery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordRecoveryRepository extends JpaRepository<PasswordRecovery, Long> {}
