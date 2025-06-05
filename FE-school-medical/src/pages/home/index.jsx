5e84ff94b1d3f9ff909a2b2fbf81afcd721260f2
Quangle-Anchor <quanglentt@gmail.com>
2025-06-05 23:56:46 +0700

    in list student part 1

--- FE-school-medical/src/pages/home/index.jsx
+++ FE-school-medical/src/pages/home/index.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import studentsApi from '../../api/studentsApi';
 import { Card } from 'antd';
 
 const HomePage = () => {
-        const{dataStudent,setDataStudent} = useState([]);
-        const fetchData = async () => {
-            try {
-                const response = await studentsApi.getStudents(); // Giả sử API trả về danh sách học sinh
-                console.log('Response:', response);
-            } catch (error) {
-                console.error('Error fetching data:', error);  
-            }
-        }
+  const [dataStudent, setDataStudent] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await studentsApi.getStudents();
+      setDataStudent(response);
+      console.log('Response:', response);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
-        useEffect(() => {
-            fetchData();
-        }, []); 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="container mx-auto p-4">
-      <div>
-        {dataStudent.map((student) => (
-          <Card hoverable
-            key={student.id}
-            title={`${student.firstName} ${student.lastName}`}
-            className="mb-4"   
-            >
-            </Card>
-        ))}
-      </div>
+    <div>
+      {dataStudent.map(student => (
+        <Card key={student.studentId} style={{ margin: '8px 0' }}>
+          <div>{student.fullName}</div>
+          <div>{student.className}</div>
+        </Card>
+      ))}
     </div>
   )
 }
