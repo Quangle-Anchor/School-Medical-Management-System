09c83e37d8666625fee575685fdfb98de7e04550
Quangle-Anchor <quanglentt@gmail.com>
2025-06-06 07:56:52 +0700

    da

--- School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/controller/StudentController.java
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/controller/StudentController.java
@@ -4,9 +4,9 @@ import com.be_source.School_Medical_Management_System_.model.Students;
 import com.be_source.School_Medical_Management_System_.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+//
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/students")
--- School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/controller/StudentHealthInfoController.java
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/controller/StudentHealthInfoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+@CrossOrigin(origins = "http://localhost:5173")
 
 @RestController
 @RequestMapping("/api/students")
