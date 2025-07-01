d285597ee93e9494bad7ffa78902e104f97a65f2
Khoa-213 <khoahass181074@fpt.edu.vn>
2025-07-01 19:09:16 +0700

    Sửa upcoming

--- School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/repository/HealthEventRepository.java
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/repository/HealthEventRepository.java
@@ -4,12 +4,12 @@ import com.be_source.School_Medical_Management_System_.model.HealthEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface HealthEventRepository extends JpaRepository<HealthEvent, Long> {
     List<HealthEvent> findByCreatedByUserId(Long userId);
     List<HealthEvent> findByCategory(String category);
-    List<HealthEvent> findByScheduleDateAfter(LocalDateTime dateTime);
+    List<HealthEvent> findByScheduleDateAfter(LocalDate dateTime);
 }
