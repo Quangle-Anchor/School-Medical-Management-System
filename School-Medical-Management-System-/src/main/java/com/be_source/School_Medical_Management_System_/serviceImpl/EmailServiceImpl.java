1d1e1e8163dfac2d4566d1ce4b0c5a855c9ed939
Khoa-213 <khoahass181074@fpt.edu.vn>
2025-07-07 12:21:38 +0700

    Fix form Mail

--- School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/serviceImpl/EmailServiceImpl.java
+++ School-Medical-Management-System-/src/main/java/com/be_source/School_Medical_Management_System_/serviceImpl/EmailServiceImpl.java
@@ -1,22 +1,31 @@
 package com.be_source.School_Medical_Management_System_.serviceImpl;
 
 import com.be_source.School_Medical_Management_System_.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
-    @Autowired
-    private JavaMailSender javaMailSender;
+
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendEmail(String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        javaMailSender.send(message);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true); // gửi HTML content
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Gửi email thất bại: " + e.getMessage());
+        }
     }
 }
